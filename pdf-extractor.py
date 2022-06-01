a7821466f704aff52bbbf6763135ec18334085a8
Deiker Diaz <deiker21diaz@gmail.com>
2022-06-01 08:18:53 -0400

    Pdf extractor

---
+++ pdf-extractor.py
@@ -0,0 +1,23 @@
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+output_string = StringIO()
+with open('pdf/The_48_Laws_of_Power.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+
+with open("txt/The_48_Laws_of_Power.txt",
+          mode="w", encoding='utf-8') as new_book:
+    new_book.write(output_string.getvalue())
+
